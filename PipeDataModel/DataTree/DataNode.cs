2885b4def5cf15dd06e245cf6afb805332bc7f9e
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-10-06 17:31:02 -0700

    Add project files.

---
+++ PipeDataModel/DataTree/DataNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipeDataModel.DataTree
+{
+    public class DataNode
+    {
+        #region-fields
+        private DataNode _parent;
+        private List<DataNode> _children;
+        #endregion
+
+        #region-properties
+        #endregion
+
+        #region-constructors
+        #endregion
+
+        #region-methods
+        #endregion
+    }
+}
---
+++ PipeDataModel/Primitives/Primitive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PipeDataModel.DataTree;
+
+namespace PipeDataModel.Primitives
+{
+    internal interface IPipeData
+    {
+        string Name { get; set; }
+        DataNode ContainerNode { get; }
+        List<string> Tags { get; }
+    }
+
+    internal abstract class PipeData<T>
+    {
+        protected T _value;
+        public T Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
