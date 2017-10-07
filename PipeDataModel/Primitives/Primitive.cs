d9b4d96d0869845b29bff9724aebd34ef6bc3efd
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-10-06 17:50:17 -0700

    Started with a new data model, still making decisions about the class architecture

--- PipeDataModel/Primitives/Primitive.cs
+++ PipeDataModel/Primitives/Primitive.cs
@@ -14,18 +14,47 @@ namespace PipeDataModel.Primitives
         List<string> Tags { get; }
     }
 
-    internal abstract class PipeData<T>
+    public abstract class PipeData<T>: IPipeData
     {
+        #region-fields
         protected T _value;
+        protected string _name;
+        protected DataNode _containerNode;
+        protected List<string> _tags;
+        #endregion
+
+        #region-properties
         public T Value
         {
             get { return _value; }
             set { _value = value; }
         }
+        #endregion
 
         public override string ToString()
         {
             return Value.ToString();
         }
+
+        #region-IPipeDataImplementation
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public DataNode ContainerNode
+        {
+            get { return _containerNode; }
+            set { _containerNode = value; }
+        }
+        public List<string> Tags
+        {
+            get
+            {
+                if ( _tags == null ) { _tags = new List<string>(); }
+                return _tags;
+            }
+        }
+        #endregion
     }
 }
