c33e562779d54bd1bb0bddf5faabc0272fe9b6d2
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-10-20 14:42:04 -0700

    Creating a new pipe over the web.

---
+++ PipeDataModel/Pipe/WebPipe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PipeDataModel.DataTree;
+
+namespace PipeDataModel.Pipe
+{
+    class WebPipe : Pipe
+    {
+        protected override DataNode PullData()
+        {
+            //ping the url and get the response
+            //deserialize response into DataNode
+            //return the DataNode
+            //incomplete
+            throw new NotImplementedException();
+        }
+
+        protected override void PushData(DataNode data)
+        {
+            //serialize the dataNode to a string
+            //send it to the url
+            //incomplete
+            throw new NotImplementedException();
+        }
+    }
+}
