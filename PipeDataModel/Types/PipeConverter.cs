4f35ed4fa013d3a0e5f8a46d24d44aaa624810cd
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-11-16 16:05:53 -0800

    started the skeleton for a standardized converter class.

--- PipeDataModel/Types/PipeConverter.cs
+++ PipeDataModel/Types/PipeConverter.cs
@@ -3,17 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PipeDataModel.DataTree;
 
 namespace PipeDataModel.Types
 {
-    public interface IPipeConverter
+    internal interface IPipeConverter
     {
-        List<Type> AllowedTypes { get; }
+        Type UserType { get; }
+        Type PipeType { get; }
     }
-    public interface IPipeConverter<T>: IPipeConverter
+    public class PipeConverter<UserT, PipeT>:IPipeConverter
+        where PipeT:IPipeMemberType
     {
-        DataNode Convert(T obj);
-        T ConvertFrom(DataNode node);
+        #region-fields
+        private Dictionary<Type, IPipeConverter> _pipeTypeMap;
+        private Dictionary<Type, IPipeConverter> _userTypeMap;
+        #endregion
+
+        #region-properties
+        public Type UserType
+        {
+            get { return typeof(UserT); }
+        }
+        public Type PipeType
+        {
+            get { return typeof(PipeT); }
+        }
+        #endregion
+
+        #region-methods
+        public void AddToPipeConverter<T1, T2>(PipeConverter<T1, T2> converter)
+            where T1:UserT
+            where T2:PipeT
+        {
+            _userTypeMap.Add(typeof(T1), converter);
+            _pipeTypeMap.Add(typeof(T2), converter);
+        }
+
+        public T2 ToPipe<T1, T2>(T1 obj)
+            where T2: PipeT
+            where T1: UserT
+        {
+            //incomplete
+            throw new NotImplementedException();
+        }
+
+        public T1 FromPipe<T1, T2>(T2 obj)
+            where T2 : PipeT
+            where T1 : UserT
+        {
+            //incomplete
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
