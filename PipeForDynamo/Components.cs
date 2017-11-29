453301528cee270953c6ad4ee03c8c36b3111204
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-11-29 01:40:07 -0800

    Added a new project for the pipe extension to Dynamo.

---
+++ PipeForDynamo/Components.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Geometry;
+
+namespace PipeForDynamo
+{
+    public class DynamoPipeComponents
+    {
+        public static object PullFromPipe(string pipeIdentifier)
+        {
+            //incomplete
+            throw new NotImplementedException();
+        }
+
+        public static object PushToPipe(string pipeIdentifier)
+        {
+            //incomplete
+            throw new NotImplementedException();
+        }
+    }
+}
