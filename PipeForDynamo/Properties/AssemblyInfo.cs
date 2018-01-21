031a11cbc579aa220af654fc710ed67510a3edc9
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2018-01-20 19:41:25 -0800

    Version updated. And Now including instructions

--- PipeForDynamo/Properties/AssemblyInfo.cs
+++ PipeForDynamo/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PipeForDynamo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ThePipe Library for Dynamo")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ranjeeth Mahankali")]
 [assembly: AssemblyProduct("PipeForDynamo")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
--- PipeForGrasshopper/Properties/AssemblyInfo.cs
+++ PipeForGrasshopper/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using Rhino.PlugIns;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PipeForGrasshopper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ThePipe plugin for Grasshopper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ranjeeth Mahankali")]
 [assembly: AssemblyProduct("PipeForGrasshopper")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -34,5 +34,5 @@ using Rhino.PlugIns;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
